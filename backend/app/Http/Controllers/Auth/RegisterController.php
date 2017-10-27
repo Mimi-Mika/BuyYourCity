00830f216972b6899fe0d99c89620d2226fdad40
Matthieu <pauly.matthieu@gmail.com>
2017-10-27 17:28:46 +0200

    fix logo.png location problem 2

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -66,12 +66,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        Mail::raw('Verify your email !!! '.PHP_EOL.'Click HERE ==> '.env('APP_URL', 'http://localhost:8000').'/api/verifyemail/'.$data['name'].'/'.Crypt::encryptString($data['name'].$data['email']), function($message) use ($data)
+        Mail::raw('Verify your email !!! '.PHP_EOL.'Click HERE ==>  :  ''http://www.api.buyyourcity.ovh/verifyemail/'.$data['name'].'/'.Crypt::encryptString($data['name'].$data['email']), function($message) use ($data)
         {
             $message->subject('Welcome to Buy Your City');
             $message->from('no.reply.byc@gmail.com', 'Admin Buy Your City');
             $message->to($data['email']);
-            $message->attach(storage_path('logo.png'));
+            $message->attach(storage_path('../logo.png'));
 
         });
         return User::create([
